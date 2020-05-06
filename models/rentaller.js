39ae1c74044fe5d8a0ae32faffe75e9d08eda690
azmprllynsa <azmi.naisa@gmail.com>
2020-05-06 21:37:44 +0700

    add table rentaller

---
+++ models/rentaller.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const rentaller = sequelize.define('rentaller', {
+    fullname: DataTypes.STRING,
+    rental_name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    address: DataTypes.STRING,
+    phone_number: DataTypes.STRING,
+    id_card: DataTypes.STRING
+  }, {});
+  rentaller.associate = function(models) {
+    // associations can be defined here
+  };
+  return rentaller;
+};
