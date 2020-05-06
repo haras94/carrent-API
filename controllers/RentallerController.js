a636dd2715ece9b359f151f6f8543275ed12ee78
azmprllynsa <azmi.naisa@gmail.com>
2020-05-06 22:08:41 +0700

    CRUD Rentaller

--- controllers/RentallerController.js
+++ controllers/RentallerController.js
@@ -1,2 +1,25 @@
 const rentaller = require('../models').rentaller
 const helpers = require('../helpers/response')
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+  registerRentaller: async (req, res) => {
+    const response = {}
+    try {
+      const salt = bcrypt.genSaltSync(10)
+
+      const data = await rentaller.create({
+        fullname: req.body.fullname,
+        rental_name: req.body.rental_name,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, salt),
+        address: req.body.address,
+        phone_number: req.body.phone_number,
+        id_card: req.body.id_card,
+        image: 'photo.jpg'
+      })
+    } catch (err) {
+
+    }
+  }
+}
